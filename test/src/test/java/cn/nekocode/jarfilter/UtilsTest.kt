ce9db1d8fc7525821df835323e3c7d69a2514bf3
shubowen <465560907@qq.com>
2026-05-11 12:42:03 +0800

    Add AGP 9 compile verification workflow

--- test/src/test/java/cn/nekocode/jarfilter/UtilsTest.kt
+++ test/src/test/java/cn/nekocode/jarfilter/UtilsTest.kt
@@ -97,7 +97,7 @@ class UtilsTest {
         } catch (e: Exception) {
             e
         }
-        assertTrue { e is TypeCastException }
+        assertTrue { e is ClassCastException }
 
         configs = Utils.getConfigsFromBytes("""
             [
